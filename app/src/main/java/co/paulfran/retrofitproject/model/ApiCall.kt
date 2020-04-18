034ebcb6d17e531cb3c6dd03da766e9d0579b65f
Paul Franco <paulfranco@me.com>
2020-04-18 06:00:04 -0700

    implement the retrofit components

---
+++ app/src/main/java/co/paulfran/retrofitproject/model/ApiCall.kt
@@ -0,0 +1,9 @@
+package co.paulfran.retrofitproject.model
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiCall {
+    @GET("apiCall")
+    fun callGet(): Call<ApiCallResponse>
+}
--- app/src/main/java/co/paulfran/retrofitproject/model/ApiCallService.kt
+++ app/src/main/java/co/paulfran/retrofitproject/model/ApiCallService.kt
@@ -1,7 +1,20 @@
 package co.paulfran.retrofitproject.model
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
 object ApiCallService {
 
     private val BASE_URL = "https://us-central1-apis2-e78c3.cloudfunctions.net/"
 
+    private val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiCall::class.java)
+
+    // expose the network communication to whoever needs it
+    fun call() =
+        api.callGet()
 }
